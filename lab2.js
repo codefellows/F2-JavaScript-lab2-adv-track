0120a3ba3a905d8af9f4516eed75a461f81adfab
Rob Merrill <robgmerrill@gmail.com>
2015-09-29 18:55:08 -0700

    lab2 advance

--- lab2.js
+++ lab2.js
@@ -43,25 +43,48 @@ function assert(expression, failureMessage) {
     fourth hour     |    4/hour        |        4         |
 
  TODO: First, make a constructor function, called Blob, that makes blobs.
-
- TODO: Next, create an instance of Blob named blob.
-
- TODO: Then, use a loop to calculate how long it took the blob to finish
- with Dowington.
 */
+  var Blob = function Blob() {
+ this.peoplePerHour = 1;
+};
+
+// TODO: Next, create an instance of Blob named blob.
+
+var blob = new Blob();
+
+//TODO: Then, use a loop to calculate how long it took the blob to finish
+// with Dowington.
+
+
 
 var hoursSpentInDowington; // TODO: assign me the value of the
                            // above calculation (how long it took
                            // the blob to eat Dowington)
+var hoursSpentInDowington = 0;
+var population = 1000;
+while (population > 0) {
+  population -= blob.peoplePerHour;
+  blob.peoplePerHour++;
+  hoursSpentInDowington
+}
+
 
 // Now, write a method that takes a population for an arbitrary
 // town, and the starting consumption rate, and returns the number
 // of hours the blob needs to ooze its way through that town.
 
 function hoursToOoze(population, peoplePerHour) {
+  var numberOfHours = 0;
+  while (population > 0) {
+    population -= peoplePerHour;
+    peoplePerHour++;
+    numberOfHours++;
+  }
+  return numberOfHours;
   // TODO: implement me based on the instructions above.
   // Be sure to then assign me to the Blob's prototype.
 }
+Blob.prototype.hoursToOoze = hoursToOoze;
 
 assert(blob.hoursToOoze(0, 1) === 0, 'no people means no time needed.');
 assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
@@ -85,13 +108,27 @@ var hello = {
 // speak, and method (that you'll place on the prototype) called
 // sayHello.
 
-function SentientBeing () {
+function SentientBeing (home, language) {
+  this.home = home;
+  this.language = language;
+
   // TODO: specify a home planet and a language
   // you'll need to add parameters to this constructor
 }
 
 // sb is a SentientBeing object
 function sayHello (sb) {
+  if (hello[this.language]) {
+    console.log(hello[this.language]);
+  } else {
+      console.log(hello['federation standard']);
+  }
+
+  if (hello[sb.language]) {
+    return hello[sb.language];
+  } else {
+      return hello['federation standard'];
+  }
     // TODO: say hello prints out (console.log's) hello in the
     // language of the speaker, but returns it in the language
     // of the listener (the sb parameter above).
@@ -99,16 +136,35 @@ function sayHello (sb) {
     // to do the translating
 
     //TODO: put this on the SentientBeing prototype
-  }
+}
+SentientBeing.prototype.sayHello = sayHello;
 
+function Human() {
+  return new SentientBeing('earth', 'federation standard');
+}
+
+function Klingon() {
+  return new SentientBeing("qo'noS", 'klingon');
+}
+
+function Romulan() {
+  return new SentientBeing('Romulus', 'romulan');
+}
 // TODO: create three subclasses of SentientBeing, one for each
 // species above (Klingon, Human, Romulan).
 
 assert((new Human()).sayHello(new Klingon()) === 'nuqneH',
   'the klingon should hear nuqneH');
 
+assert((new Human()).sayHello(new Romulan()) === 'Jolan\'tru',
+  'the romulan should hear Jolan\'tru');
+
+assert((new Klingon()).sayHello(new Human()) === 'hello',
+  'the human should hear hello')
 // TODO: write five more assertions, to complete all the possible
 // greetings between the three types of sentient beings you created above.
+
+
 
 //*********************************************************
 // PROBLEM 3: Sorting. 20 points.
@@ -120,28 +176,36 @@ assert((new Human()).sayHello(new Klingon()) === 'nuqneH',
 
 function lastLetterSort(stringArray) {
   function byLastLetter(a, b) {
-    //TODO: implement me. sort the strings in alphabetical
-    // order using their last letter
-    // Read this about how the sort function works:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-    // this byLastLetter function is a "compare function"
-    // And check out the "comparing strings" section  here:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+    var a = a[a.length - 1].toLowerCase(),
+            b = b[b.length - 1].toLowerCase();
+      if(a < b) {
+          return -1;
+        } else if(a > b) {
+          return 1;
+        }
+
+        return 0;
+    }
+    stringArray.sort(byLastLetter);
   }
-  stringArray.sort(byLastLetter);
-}
 
 function sumArray(numberArray) {
   var sum = 0;
-  // TODO: implement me using forEach
+   numberArray.forEach(function(number) {
+      sum += number;
+    });
   return sum;
 }
 
 function sumSort(arrayOfArrays) {
-  arrayOfArrays.sort(function(item) {
-    // TODO: implement me using sumArray
-    //  order the arrays based on the sum of the numbers
-    //  inside each array
+  arrayOfArrays.sort(function(a, b) {
+    if(a < b) {
+          return -1;
+        } else if(a > b) {
+          return 1;
+        }
+
+        return 0;
   });
 }
 
@@ -152,3 +216,4 @@ function sumSort(arrayOfArrays) {
 // ./node_modules/.bin/grunt jshint
 // ./node_modules/.bin/grunt jscs
 //*********************************************************
+
